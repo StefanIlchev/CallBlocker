5e9b9fc555d6ebb2c03a84d1b9cbdee1e6ec830d
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2024-04-11 22:48:25 +0300

    Update CallTest.kt

--- CallBlocker/src/androidTest/kotlin/ilchev/stefan/callblocker/test/CallTest.kt
+++ CallBlocker/src/androidTest/kotlin/ilchev/stefan/callblocker/test/CallTest.kt
@@ -35,8 +35,11 @@ class CallTest(
 	private fun assertBlocked(context: Context) {
 		val sharedPreferences = context.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE)
 		val blockPredicate = BlockPredicate(sharedPreferences) { isContact }
-		val isBlockedActual = blockPredicate(incomingNumber)
-		Assert.assertEquals(isBlockedExpected, isBlockedActual)
+		Assert.assertEquals(isBlockNonContacts, blockPredicate.isBlockNonContacts)
+		Assert.assertEquals(isExcludeContacts, blockPredicate.isExcludeContacts)
+		Assert.assertEquals(regex, blockPredicate.regex)
+		Assert.assertEquals(isMatches, blockPredicate.isMatches)
+		Assert.assertEquals(isBlockedExpected, blockPredicate(incomingNumber))
 	}
 
 	@Test
@@ -74,13 +77,13 @@ class CallTest(
 			arrayOf(false, false, "a", null, "b", null, false),
 			arrayOf(false, false, "a", null, "b", true, false),
 			arrayOf(false, false, "a", null, "b", false, false),
-			arrayOf(false, false, "a", true, "a", null, false),
+			arrayOf(false, false, "a", true, "a", null, true),
 			arrayOf(false, false, "a", true, "a", true, true),
 			arrayOf(false, false, "a", true, "a", false, true),
 			arrayOf(false, false, "a", true, "b", null, false),
 			arrayOf(false, false, "a", true, "b", true, false),
 			arrayOf(false, false, "a", true, "b", false, false),
-			arrayOf(false, false, "a", false, "a", null, true),
+			arrayOf(false, false, "a", false, "a", null, false),
 			arrayOf(false, false, "a", false, "a", true, false),
 			arrayOf(false, false, "a", false, "a", false, false),
 			arrayOf(false, false, "a", false, "b", null, true),
@@ -92,13 +95,13 @@ class CallTest(
 			arrayOf(false, true, "a", null, "b", null, false),
 			arrayOf(false, true, "a", null, "b", true, false),
 			arrayOf(false, true, "a", null, "b", false, false),
-			arrayOf(false, true, "a", true, "a", null, false),
+			arrayOf(false, true, "a", true, "a", null, true),
 			arrayOf(false, true, "a", true, "a", true, false),
 			arrayOf(false, true, "a", true, "a", false, true),
 			arrayOf(false, true, "a", true, "b", null, false),
 			arrayOf(false, true, "a", true, "b", true, false),
 			arrayOf(false, true, "a", true, "b", false, false),
-			arrayOf(false, true, "a", false, "a", null, true),
+			arrayOf(false, true, "a", false, "a", null, false),
 			arrayOf(false, true, "a", false, "a", true, false),
 			arrayOf(false, true, "a", false, "a", false, false),
 			arrayOf(false, true, "a", false, "b", null, true),
@@ -110,13 +113,13 @@ class CallTest(
 			arrayOf(true, false, "a", null, "b", null, false),
 			arrayOf(true, false, "a", null, "b", true, false),
 			arrayOf(true, false, "a", null, "b", false, true),
-			arrayOf(true, false, "a", true, "a", null, false),
+			arrayOf(true, false, "a", true, "a", null, true),
 			arrayOf(true, false, "a", true, "a", true, true),
 			arrayOf(true, false, "a", true, "a", false, true),
 			arrayOf(true, false, "a", true, "b", null, false),
 			arrayOf(true, false, "a", true, "b", true, false),
 			arrayOf(true, false, "a", true, "b", false, true),
-			arrayOf(true, false, "a", false, "a", null, true),
+			arrayOf(true, false, "a", false, "a", null, false),
 			arrayOf(true, false, "a", false, "a", true, false),
 			arrayOf(true, false, "a", false, "a", false, true),
 			arrayOf(true, false, "a", false, "b", null, true),
@@ -128,13 +131,13 @@ class CallTest(
 			arrayOf(true, true, "a", null, "b", null, false),
 			arrayOf(true, true, "a", null, "b", true, false),
 			arrayOf(true, true, "a", null, "b", false, true),
-			arrayOf(true, true, "a", true, "a", null, false),
+			arrayOf(true, true, "a", true, "a", null, true),
 			arrayOf(true, true, "a", true, "a", true, false),
 			arrayOf(true, true, "a", true, "a", false, true),
 			arrayOf(true, true, "a", true, "b", null, false),
 			arrayOf(true, true, "a", true, "b", true, false),
 			arrayOf(true, true, "a", true, "b", false, true),
-			arrayOf(true, true, "a", false, "a", null, true),
+			arrayOf(true, true, "a", false, "a", null, false),
 			arrayOf(true, true, "a", false, "a", true, false),
 			arrayOf(true, true, "a", false, "a", false, true),
 			arrayOf(true, true, "a", false, "b", null, true),
