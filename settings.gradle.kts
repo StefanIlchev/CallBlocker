1472132837f80acb5cf55ac21578713b14775fc3
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2024-05-10 23:22:50 +0300

    Version 27

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ dependencyResolutionManagement {
 
 		create("libs") {
 
-			version("versionCode", "26")
+			version("versionCode", "27")
 
 			// https://developer.android.com/build/jdks
 			version("jvmToolchain", "21")
