1fca39e43d28ec078303f456e3a9bcb932e87251
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2025-09-07 18:13:47 +0300

    Version 40

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
 
 		create("libs") {
 
-			version("versionCode", "39")
+			version("versionCode", "40")
 
 			// https://developer.android.com/build/jdks
 			version("jvmToolchain", "21")
